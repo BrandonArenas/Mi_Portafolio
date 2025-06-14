7ea0c54e9a18c6fb054bc27dd524e6bb37e5e407
BrandonArenas <arenasbandabrandon@gmail.com>
2025-06-14 17:20:12 -0500

    Guardar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <section id="about" className="section-padding">
       <h2 className="section-title">Sobre Mí</h2>
       <div className="about-content">
-        <img src="/imgs/profile.jpeg" alt="Foto de perfil" className="profile-pic" />
+        <img src="imgs/profile.jpeg" alt="Foto de perfil" className="profile-pic" />
         <div className="about-text">
           <p>Soy desarrollador de software junior con experiencia en el desarrollo de aplicaciones web y el uso de sistemas empresariales. He trabajado con tecnologías frontend como HTML, CSS, JavaScript, así como con PHP, MySQL o SQLServer en el backend.</p>
           <p>Durante mis experiencias, he contribuido en proyectos, desarrollando interfaces web y backend, automatizando procesos con Python y macros VBA, y gestionando datos en sistemas ERP. Me considero proactivo, con facilidad para aprender nuevas tecnologías y adaptarme a diferentes entornos de trabajo. Mi objetivo es seguir creciendo como desarrollador y aportar soluciones funcionales y eficientes.</p>
